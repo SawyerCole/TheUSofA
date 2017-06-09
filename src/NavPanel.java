4e5535d1e01c4aceaa5501cd7509c503e72aea45
Victor  Shan <lninjavs@gmail.com>
2017-06-08 22:51:06 -0700

    Major Fixes
    Added StatesAndAbbreviations.txt
    
    Added a Navigation Panel (NavPanel)
    
    Deleted DrawingPanel
    
    Smoothed out all the classes to run smoothly
    
    Still no picture though
    
    Signed-off-by: Victor <lninjavs@gmail.com>

---
+++ src/NavPanel.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+
+/**
+ * NavPanel.java
+ * Assignment:
+ * Purpose:
+ *
+ * @version 6/8/2017
+ */
+public class NavPanel extends JPanel {
+    JLabel stateLabel, countyLabel;
+    JTextField stateTextField, countyTextField;
+    JCheckBox isCounty;
+    JButton viewCountry, submitButton;
+    Outline outline;
+
+    public NavPanel(Outline outline) {
+        this.outline = outline;
+        createComponents();
+        addComponents();
+        componentsActions();
+    }
+
+    // Creates navigation bar components
+    private void createComponents() {
+        viewCountry = new JButton("View Country");
+        stateLabel = new JLabel("State: ");
+        stateTextField = new JTextField(15);
+        countyLabel = new JLabel("County: ");
+        countyTextField = new JTextField(15);
+        countyTextField.setEnabled(false);
+        isCounty = new JCheckBox("This is a County");
+        submitButton = new JButton("Submit");
+    }
+
+    // Creates navigation bar's action(s)
+    private void componentsActions() {
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    outline.fileInput(stateTextField.getText());
+                    outline.fileToDrawing();
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        viewCountry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    outline.fileInput("USA");
+                    outline.fileToDrawing();
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
+
+        isCounty.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                countyTextField.setEnabled(isCounty.isSelected());
+            }
+        });
+    }
+
+    // adds navigation bar components to the panel
+    private void addComponents() {
+        add(viewCountry);
+        add(stateLabel);
+        add(stateTextField);
+        add(isCounty);
+        add(countyLabel);
+        add(countyTextField);
+        add(submitButton);
+    }
+
+}
--- src/Outline.java
+++ src/Outline.java
@@ -1,125 +1,147 @@
-import java.io.*;
-import java.util.*;
-import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Outline{
+public class Outline {
     //Sets a scaling factor so the states can be scaled however the user chooses.
-    public static final int SCALE = 20;
-    public String subregionName;
-    public String regionName;
-    public int[] points = new int[4];
+    private static final int SCALE = 20;
+    private String subregionName;
+    private String stateName;
+    private ArrayList <int[]> lines = new ArrayList();
 
-    public int[] getPoints() {
-        return points;
+
+    Outline() {
     }
 
-    //Allows the user to enter
-    public Outline(String subregionName, String regionName, int[] points) {
-        this.subregionName = subregionName;
-        this.regionName = regionName;
-        this.points = points;
-    }
+//    //Allows the user to enter
+//    Outline(String subregionName, String stateName, int[] lines) {
+//        this.subregionName = subregionName;
+//        this.stateName = stateName;
+//        this.lines = lines;
+//    }
+
     //The intro text that explains the program
-    public void intro(){
+    public void intro() {
         System.out.println("This program draws a visualization of");
         System.out.println("data from a given file. Enter");
         System.out.println("- the 2 letter abbreviation for each state");
-        System.out.println("- USA for all of the US by state, or");
-        System.out.println("- USA-county for all of the US by county.");
+        System.out.println("- USA for all of the US by state.");
         System.out.println("Capitalization does not matter.");
         System.out.println();
     }
+
     //Allows the user to input what they want to see (a state, the country or the country by county) and it will give the file of the chosen image to the next computer
-    public Scanner fileInput(){
-        Scanner files = new Scanner(System.in);
-        boolean fileChosen = false;
-        while(fileChosen == false){
-            System.out.println("What would you like to see?");
-            String fileToView = files.next() + ".txt";
-            try{
-                files = new Scanner(new File("src\\data\\" + fileToView));
-                fileChosen = true;
-            }catch(FileNotFoundException e){
-                System.out.println("File not found. Try again.");
-                fileChosen = false;
+    public void fileInput(String name) throws FileNotFoundException {
+        Scanner file = new Scanner(new File("src/Data/StatesAndAbbreviations"));
+        boolean correctRegion = false;
+        while (file.hasNext() && !correctRegion) {
+            String state = file.next();
+            if (state.equalsIgnoreCase("New")) {
+                state += " " + file.next();
+            }
+            if (name.equalsIgnoreCase(state)) {
+                correctRegion = true;
+                if (name.length() > 2 && !name.equalsIgnoreCase("USA") || name.equalsIgnoreCase("US")) {
+                    state = file.next();
+                    this.stateName = state.toUpperCase();
+                } else {
+                    this.stateName = state.toUpperCase();
+                }
             }
         }
-        return files;
+//        while(fileChosen == false){
+//            System.out.println("What would you like to see?");
+//            String fileToView = name + ".txt";
+//            try{
+//                file = new Scanner(new File("src\\data\\" + fileToView));
+//                fileChosen = true;
+//            }catch(FileNotFoundException e){
+//                System.out.println("File not found. Try again.");
+//                fileChosen = false;
+//            }
+//        }
     }
-    //Draws the lines outlining the states and counties using the latitude and longitude points from the file of the chosen state
-    public Scanner fileToDrawing(Scanner files){
-        double minLong = files.nextDouble();
-        double minLat = files.nextDouble();
-        double maxLong = files.nextDouble();
-        double maxLat = files.nextDouble();
+
+    //Draws the lines outlining the states and counties using the latitude and longitude lines from the file of the chosen state
+    public void fileToDrawing() throws FileNotFoundException {
+        Scanner file = new Scanner(new File("src\\data\\" + stateName + ".txt"));
+        double minLong = file.nextDouble();
+        double minLat = file.nextDouble();
+        double maxLong = file.nextDouble();
+        double maxLat = file.nextDouble();
         //I added 100 on each side to allow the image to be centered better so the outline of the state could be seen more clearly
-        DrawingPanel panel = new DrawingPanel( (int) ((maxLong - minLong) * SCALE) + (SCALE * 2), (int) ((maxLat - minLat) * SCALE) + (SCALE * 2));
-        Graphics g = panel.getGraphics();
+//        DrawingPanel panel = new DrawingPanel((int) ((maxLong - minLong) * SCALE) + (SCALE * 2), (int) ((maxLat - minLat) * SCALE) + (SCALE * 2));
+//        Graphics g = panel.getGraphics();
         double lat1;
         double long1;
         double lat2;
         double long2;
-        int subregions = files.nextInt();
+        int subregions = file.nextInt();
         //After the first 5 different variables everything repeats in the same manner so it will repeat this way with every subregion
-        while(files.hasNext() == true){
+        while (file.hasNext()) {
+
             //Used to skip the gap between subregions
-            files.next();
-            String subregionName = files.nextLine();
-            String regionName = files.next();
-            int points = files.nextInt();
+            file.next();
+            String subregionName = file.nextLine();
+            String regionName = file.next();
+            int points = file.nextInt();
             //Added the scale so that the state/country would be better centered
-            long1 = (files.nextDouble() - minLong) * SCALE + SCALE;
-            lat1 = (maxLat - files.nextDouble()) * SCALE + SCALE;
+            long1 = (file.nextDouble() - minLong) * SCALE + SCALE;
+            lat1 = (maxLat - file.nextDouble()) * SCALE + SCALE;
             //Subtracted 1 because the first line is already dealt with above
-            for(int i = 1; i <= points - 1; i++){
+            for (int i = 1; i <= points - 1; i++) {
                 //I have these if statements to make sure that the decimal point is accounted for as turning a double into an int always rounds down
-                if(long1 % 1 > 0.5){
+                if (long1 % 1 > 0.5) {
                     long1 = long1 / 1 + 1;
                 }
-                if(lat1 % 1 > 0.5){
+                if (lat1 % 1 > 0.5) {
                     lat1 = lat1 / 1 + 1;
                 }
                 //Added the scale so the state would be better centered
-                long2 = (files.nextDouble()- minLong) * SCALE + SCALE;
-                lat2 = (maxLat - files.nextDouble()) * SCALE + SCALE;
+                long2 = (file.nextDouble() - minLong) * SCALE + SCALE;
+                lat2 = (maxLat - file.nextDouble()) * SCALE + SCALE;
                 //I have these if statements to make sure that the decimal point is accounted for as turning a double into an int always rounds down
-                if(long2 % 1 > 0.5){
+                if (long2 % 1 > 0.5) {
                     long2 = long2 / 1 + 1;
                 }
-                if(lat2 % 1 > 0.5){
+                if (lat2 % 1 > 0.5) {
                     lat2 = lat2 / 1 + 1;
                 }
-                //Turned the different latitude and longitude points into ints because drawings can't use doubles
-                this.points[0] = (int) lat2;
-                this.points[1] = (int) long2;
-                this.points[2] = (int) lat1;
-                this.points[3] = (int) long1;
+                //Turned the different latitude and longitude lines into ints because drawings can't use doubles
+                int[] line = new int[4];
+                line[0] = (int) lat2;
+                line[1] = (int) long2;
+                line[2] = (int) lat1;
+                line[3] = (int) long1;
+                lines.add(line);
                 long1 = long2;
                 lat1 = lat2;
             }
         }
-        return files;
+
     }
+
     //Used to see if the user wants to continue or not
-    public boolean toContinue(Scanner files){
-        files = new Scanner(System.in);
+    public boolean toContinue() {
+        Scanner files = new Scanner(System.in);
         System.out.print("Would you like to view somewhere new?");
         String viewNew = files.next();
         System.out.println();
         boolean response = false;
-        while(response == false){
-            if(viewNew.charAt(0)==('y') || viewNew.charAt(0) == ('Y')|| viewNew.charAt(0) == ('n') || viewNew.charAt(0) == ('N')){
+        while (!response) {
+            if (viewNew.charAt(0) == ('y') || viewNew.charAt(0) == ('Y') || viewNew.charAt(0) == ('n') || viewNew.charAt(0) == ('N')) {
                 response = true;
-            }
-            else{
+            } else {
                 System.out.println("I didn't understand that.");
             }
         }
-        if(viewNew.charAt(0)==('y') || viewNew.charAt(0) == ('Y')){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return viewNew.charAt(0) == ('y') || viewNew.charAt(0) == ('Y');
+    }
+
+
+    // Getters and Setters
+    public ArrayList <int[]> getLines() {
+        return lines;
     }
 }
